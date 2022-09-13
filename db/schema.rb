36445a516e3d85dba5d1910768289de490b1a876
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2022-09-13 12:33:32 +0000

    [Update]

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_11_092708) do
+ActiveRecord::Schema.define(version: 2022_09_11_095609) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
